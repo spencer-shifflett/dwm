e9d6b5bc0cf23dcfc2ce386415b6d0a89aee366a
spencer-shifflett <spencer@sshifflett.com>
2021-11-01 18:39:57 +0000

    Add audio

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -64,6 +64,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          SHCMD("kitty") },
+	{ MODKEY,			XK_equal,  spawn,	   SHCMD("pactl -- set-sink-volume 0 +5%") },
+	{ MODKEY,                       XK_minus,  spawn,          SHCMD("pactl -- set-sink-volume 0 -5%") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
--- slstatus/config.h
+++ slstatus/config.h
@@ -66,6 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ ipv4,     "[ens33 : %s] ", "ens33"},
+	{ run_command, "[VOL : %s] ", "pactl list sinks | grep 'Volume: front-left' | awk '{print $5}'"},
         { cpu_perc, "[CPU : %s%] ", NULL  },
         { ram_perc, "[RAM : %s%] ", NULL  },
 	{ datetime, "%s",           "%b %d, %Y %I:%M%p" },
