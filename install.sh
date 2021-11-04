0d22c8f97333f62bc5fd8c2d54b6fd176d33b814
spencer-shifflett <spencer@sshifflett.com>
2021-11-04 15:25:22 -0400

    added && and prompts

--- install.sh
+++ install.sh
@@ -3,11 +3,17 @@ sudo apt update
 sudo apt full-upgrade -y
 xargs sudo apt install -y <requirements.txt
 cd dmenu-5.0
-sudo make clean install
+echo
+echo "Building DMenu..."
+sudo make clean install &&
 cd ../dwm-6.2
-sudo make clean install
+echo
+echo "Building DWM..."
+sudo make clean install &&
 cd ../slstatus
-sudo make clean install
+echo
+echo "Building SlStatus..."
+sudo make clean install &&
 cd ../
 cp xinitrc ~/.xinitrc
 sudo cp ./vol /bin/
